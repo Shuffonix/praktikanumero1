4e24af4cf4e5813f8a1be14b289fafdeecd4eb0d
Shuffonix <maetrevor@gmail.com>
2023-02-16 01:35:09 +0200

    lisasin seletused

--- main.py
+++ main.py
@@ -4,18 +4,31 @@ from Block import Block
 
 screen = pygame.display.set_mode((640, 480))
 kell = pygame.time.Clock()
-running = True
-global_x = 0
-global_y = 0
 speed = 100
 in_range = False
-local_x = 0
-local_y = 0
+running = True
+global_x = 0  # x mis ei liigu mapi suhtes
+global_y = 0  # y mis ei liigu mapi suhtes
+local_x = 0  # 40ga ümardatud x ekraani suhtes
+local_y = 0  # 40ga ümardatud y ekraani suhtes
+selected_x = 0  # local_x aga mapi suhtes
+selected_y = 0  # local_y aga mapi suhtes
+
+# pmst (local_x, local_y) valikukasti jaoks,
+# global_x/y + local_x/y = selected_x/y,
+# (selected_x, selected_y) ülejäänud blockide jaoks
+
+# kuna mängija püsib paigal ja map liigub,
+# igasugused kiirused mängija pos liitmise asemel lahutatakse globalist
+# (mängija "edasi liikumise" kujutamiseks liigub map tagasi)
+
+# hiirt järgiv valikukast
 selected_block = pygame.Surface(size=(42, 42))
 selected_block.fill((255, 0, 0))
 selected_block.set_colorkey((255, 0, 0))
 pygame.draw.rect(selected_block, (0, 0, 0), (0, 0, 42, 42), 3)
 
+# genereerib mapi
 blocks_dict = {}
 blocks = pygame.sprite.Group()
 for y in range(320, 2300, 40):
@@ -24,13 +37,16 @@ for y in range(320, 2300, 40):
         blocks_dict[block.id] = block
         blocks.add(block)
 
+# update(...) - uuendab blockide asukohti
+# update(..., True) - uuendab lisaks ka naabreid (kasulik, kui lisad või eemaldad blocke)
 blocks.update(global_x, global_y, blocks_dict, True)
+
 while running:
     hiire_x, hiire_y = pygame.mouse.get_pos()
     dt = kell.tick(144) / 1000
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_d]:
+    if keys[pygame.K_d]:  # liigu
         global_x += speed * dt
     if keys[pygame.K_a]:
         global_x -= speed * dt
@@ -59,6 +75,7 @@ while running:
     selected_x = 40 * round(int(global_x + local_x) / 40)
     selected_y = 40 * round(int(global_y + local_y) / 40)
 
+    # raadius ekraani keskelt hiireni <= 200
     if ((hiire_x - 320)**2 + (hiire_y - 240)**2)**0.5 <= 200:
         in_range = True
     else:
@@ -68,11 +85,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # lõhub blocke
     if left_click:
         if (selected_x, selected_y) in blocks_dict and in_range:
             blocks_dict[(selected_x, selected_y)].kill()
             blocks_dict.pop((selected_x, selected_y))
             blocks.update(global_x, global_y, blocks_dict, True)
+
+    # lisab blocke
     elif right_click:
         if (not (selected_x, selected_y) in blocks_dict) and in_range:
             block = Block(selected_x, selected_y)
@@ -91,7 +111,8 @@ while running:
     blocks.update(global_x, global_y, blocks_dict)
     blocks.draw(screen)
 
-    # range check
+    # in range - valikukast
+    # not in range - range circle
     if not in_range:
         pygame.draw.circle(screen, (125, 125, 125), (320, 240), 200, 2)
     elif (selected_x, selected_y) in blocks_dict:
