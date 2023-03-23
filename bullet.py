ba13c9c488bdbd33d551a68a7329ac4068f5e94c
Rbr4t <robertkoor0@gmail.com>
2023-03-23 16:58:38 +0200

    esimene commit uuel branchil

--- bullet.py
+++ bullet.py
@@ -5,17 +5,19 @@ from math import atan2, degrees, cos, sin, radians
 # sry ma ei saa sellest Group asjast ikka veel aru, seega ma ei kasuta toda lol
 class Bullet():
     def __init__(self, x, y, rad):
-        self.image = pygame.Surface((10, 10))
+
         self.velocity = 100
         self.dx = cos(rad) * self.velocity
         self.dy = -sin(rad) * self.velocity  # fuck põdra ja fuck matemaatika bruh
         self.x = x
         self.y = y
+        self.image = pygame.Rect((self.x, self.y, 10, 10))
         self.rad = rad
 
     def update(self, dt):
         self.x = self.x + self.dx * dt
         self.y = self.y + self.dy * dt
+        self.image = pygame.Rect((self.x, self.y, 10, 10))
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (255, 0, 0), [self.x, self.y], 10)
+        pygame.draw.rect(screen, (255, 0, 0), self.image)
