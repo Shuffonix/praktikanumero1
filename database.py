56180a008bbb444410decc59652408add72dd382
Rbr4t <robertkoor0@gmail.com>
2023-04-16 20:46:03 +0300

    andmebaasi connection ja vajalik script andmete kätte saamiseks

---
+++ database.py
@@ -0,0 +1,40 @@
+import psycopg2
+
+# establishing the connection
+connection = psycopg2.connect(
+    database="pygame_project",
+    user='pygame_project_user',
+    password='IZT3VvhXwMxwKD81HyoOQ7MPbUFASwJE',
+    host='dpg-cgu20a02qv2fdeb7n9qg-a.frankfurt-postgres.render.com',
+    port='5432'
+)
+
+# andmebaasi loomiseks
+cursor = connection.cursor()
+
+create_db = '''CREATE TABLE SCORES(
+   NAME VARCHAR(255) NOT NULL,
+   SCORE INT   
+)'''
+
+# cursor.execute(create_db)
+# connection.commit()
+# cursor.close()
+
+
+def add_player(name, score):
+    cursor = connection.cursor()
+    script = "INSERT INTO SCORES(NAME, SCORE) VALUES (%s, %s)"
+    cursor.executemany(script, [name, score])
+    connection.commit()
+    cursor.close()
+
+
+def query_data():
+    cursor = connection.cursor()
+    query = """SELECT * FROM SCORES ORDER BY SCORE DESC LIMIT 10;"""
+    cursor.execute(query)
+    connection.commit()
+    resp = cursor.fetchall()
+    cursor.close()
+    return resp
